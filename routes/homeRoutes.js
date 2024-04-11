8ffe301a9307bfd7e72b032c067f0889fb866aa3
Brandon Butler <85582593+BrandonButler123@users.noreply.github.com>
2024-04-11 17:35:44 -0230

    initializing mongoDB connection
    setting up the connection for mongoDB

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const dataAccessLayer = require("../controllers/DAL.js");
+const dataAccessLayer = require("../controllers/pgDAL.js");
 
 router.get("/", async (req, res) => {
   res.render("home");
