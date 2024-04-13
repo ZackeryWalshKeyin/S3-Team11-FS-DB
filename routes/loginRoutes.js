f431123bc042e676c50ae7830815037cdbf8704e
willoldford <william.oldford@hotmail.com>
2024-04-13 17:04:38 -0230

    Added login/signup features with passport

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -1,20 +1,45 @@
 const express = require("express");
 const router = express.Router();
-const dataAccessLayer = require("../controllers/pgDAL.js");
+const passport = require("passport");
+const localStrategy = require("passport-local").Strategy;
+
+const logins = require("../controllers/pgLoginsDAL.js");
+
+// Initialize Passport
+router.use(passport.initialize());
+router.use(passport.session());
+
+// Passport Local authentication setup
+passport.use(
+  new localStrategy(
+    { usernameField: "username" },
+    async (username, password, done) => {
+      let user = await logins.getLoginByUsername(username);
+      if (!user) {
+        return done(null, false, { message: "No user with that username." });
+      }
+      try {
+        if (password === user.password) {
+          return done(null, user);
+        } else {
+          return done(null, false, { message: "Incorrect password." });
+        }
+      } catch (error) {
+        return done(error);
+      }
+    }
+  )
+);
 
 router.get("/", (req, res) => {
-  res.render("login");
+  res.render("login", { message: req.query.message });
 });
-
-router.post("/", (req, res) => {
-  const { username, password } = req.body;
-
-  // NEED TO CHECK USERNAME AND PASSWORD AGAINST DATABASE HERE - FOR NOW HARDCODED - TO BE COMPLETED
-  if (username === "admin" && password === "password") {
-    res.send("Login successful");
-  } else {
-    res.send("Invalid username or password");
-  }
-});
+router.post(
+  "/",
+  passport.authenticate("local", {
+    successRedirect: "/home", // Redirect to the home page if authentication is successful
+    failureRedirect: "/login?message=Invalid%20username%20or%20password",
+  })
+);
 
 module.exports = router;
